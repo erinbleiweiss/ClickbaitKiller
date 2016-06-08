321a960f347ef6babd2b91c7efb1c1a63ad242b4
Erin Bleiweiss <erinbleiweiss@gmail.com>
2016-06-08 18:54:03 -0500

    generate regex for spelled numerals

--- js/content.js
+++ js/content.js
@@ -1,7 +1,6 @@
 var clickbait_phrases = [
     "Bernie Sanders",
     "(will|may) surprise you",
-    "\d ((stupid|dumb)\s)*reason",
 
     "find out why",
 
@@ -11,7 +10,6 @@ var clickbait_phrases = [
     "ranking",
     "will surprise you",
     "find out why",
-    "\\d+\\w*reason",
     "this common",
     "\\d+ surprising",
     "\\d+ of the best",
@@ -28,6 +26,40 @@ var clickbait_phrases = [
 //        remove_clickbait()
 //    }
 //});
+
+
+
+clickbait_phrases.push(spell_numbers("NUMBER ((stupid|dumb)\\s)*reason(s)*"));
+
+
+// Regex should contain "NUMBER", signifying a numeral.  This method adds spelled-out numbers 0-20 as regex keywords.
+// Final regex will be in the format "(one|two|three|\d)-*"
+// The hyphen should account for numbers in the form "twenty-two"
+function spell_numbers(regex){
+
+    var numbers = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    var number_string = "(";
+
+    for (var n=0; n<numbers.length; n++){
+        number_string += numbers[n];
+        if (n != numbers.length -1) {
+            number_string += "|"
+        } else {
+            number_string += "|\\d)-*"
+        }
+    }
+
+    return regex.replace("NUMBER", number_string);
+}
+
+
+
+
 
 
 remove_clickbait();
