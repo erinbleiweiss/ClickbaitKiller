4c0b81f05a0ff4fd060354c957debd8385e3457e
Erin Bleiweiss <erinbleiweiss@gmail.com>
2016-06-08 19:58:36 -0500

    generic synonym replacement

--- js/content.js
+++ js/content.js
@@ -20,6 +20,12 @@ var clickbait_phrases = [
     "you have to watch"
 ];
 
+var ADJECTIVES = [
+    'stupid',
+    'dumb',
+    'crazy'
+];
+
 //chrome.storage.sync.get(function (data) {
 //    var isToggled = data.toggle;
 //    if (isToggled){
@@ -29,39 +35,52 @@ var clickbait_phrases = [
 
 
 
-clickbait_phrases.push(spell_numbers("NUMBER ((stupid|dumb)\\s)*reason(s)*"));
-
-
 // Regex should contain "NUMBER", signifying a numeral.  This method adds spelled-out numbers 0-20 as regex keywords.
 // Final regex will be in the format "(one|two|three|\d)-*"
 // The hyphen should account for numbers in the form "twenty-two"
-function spell_numbers(regex){
-
+String.prototype.spell_numbers = function(){
     var numbers = [
-        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
-        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
         'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
     ];
-
     var number_string = "(";
-
-    for (var n=0; n<numbers.length; n++){
-        number_string += numbers[n];
-        if (n != numbers.length -1) {
+    for (var i=0; i<numbers.length; i++){
+        number_string += numbers[i];
+        if (i != numbers.length-1) {
             number_string += "|"
         } else {
             number_string += "|\\d)-*"
         }
     }
+    return this.replace("NUMBER", number_string);
+};
 
-    return regex.replace("NUMBER", number_string);
+
+// Regex should contain "KEY", signifying the keyword to be replaced.
+// Final regex will be in the format "(synonym|alternative|substitute)"
+String.prototype.synonyms = function(synonyms, key) {
+    var synonym_string = "(";
+    for (var i=0; i<synonyms.length; i++){
+        synonym_string += synonyms[i];
+        if (i != synonyms.length -1) {
+            synonym_string += "|"
+        } else {
+            synonym_string += ")"
+        }
+    }
+    return this.replace(key, synonym_string);
+};
+
+
+function filter_and_push(regex){
+    regex = regex.spell_numbers();
+    regex = regex.synonyms(ADJECTIVES, "ADJ");
+    clickbait_phrases.push(regex);
 }
 
 
-
-
-
-
+filter_and_push("NUMBER (ADJ\\s)*reason(s)*");
 remove_clickbait();
 
 function remove_clickbait(){
